1680803e0aac52ceba020ec7e55677b4da7a323f
jskwon-eks-project <jskwon20@icloud.com>
2025-12-22 13:21:21 +0900

    ADD: terraform file

--- def.tf
+++ def.tf
@@ -2,3 +2,8 @@ resource "local_file" "def" {
   content  = "def!"
   filename = "${path.module}/def.txt"
 }
+
+resource "local_file" "def2" {
+  content  = "def2!"
+  filename = "${path.module}/def2.txt"
+}
